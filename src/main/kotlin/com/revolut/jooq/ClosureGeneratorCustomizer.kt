53571e3a5ed34fd98802575f26ac21f5d2d76e36
Adrian Skrobacz <adrian.skrobacz@revolut.com>
2019-06-20 23:53:34 +0200

    Support for Groovy Closures for generator customization within task

---
+++ src/main/kotlin/com/revolut/jooq/ClosureGeneratorCustomizer.kt
@@ -0,0 +1,22 @@
+package com.revolut.jooq
+
+import groovy.lang.Closure
+import groovy.lang.DelegatingMetaClass
+import groovy.lang.GroovyObject
+import org.codehaus.groovy.runtime.wrappers.PojoWrapper
+import org.jooq.meta.jaxb.Generator
+
+class ClosureGeneratorCustomizer(closure: Closure<Generator>) : GeneratorCustomizer {
+    private val closure = closure.dehydrate();
+
+    override fun execute(generator: Generator) {
+        val groovyWrappedGenerator = generator.toGroovyObject()
+        closure.rehydrate(groovyWrappedGenerator, groovyWrappedGenerator, groovyWrappedGenerator).call(groovyWrappedGenerator)
+    }
+
+    private fun Generator.toGroovyObject(): GroovyObject {
+        val groovyObject = PojoWrapper(this, this::class.java)
+        groovyObject.metaClass = DelegatingMetaClass(this::class.java)
+        return groovyObject
+    }
+}
