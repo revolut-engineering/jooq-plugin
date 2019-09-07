d716bd5f0ef66749520817c29c399a631b1e7496
Adrian Skrobacz <adrian.skrobacz@revolut.com>
2019-09-07 12:11:43 +0200

    Do not apply Java plugin by default, let the user choose

--- src/main/kotlin/com/revolut/jooq/JooqDockerPlugin.kt
+++ src/main/kotlin/com/revolut/jooq/JooqDockerPlugin.kt
@@ -2,12 +2,10 @@ package com.revolut.jooq
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.plugins.JavaPlugin
 
 open class JooqDockerPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
-        project.pluginManager.apply(JavaPlugin::class.java)
         project.extensions.create("jooq", JooqExtension::class.java)
         project.configurations.create("jdbc")
         project.tasks.create("generateJooqClasses", GenerateJooqClassesTask::class.java) {
