721b0ed2bdc85f43f8eee6e6ae3a60171fc363b0
Adrian Skrobacz <adrian.skrobacz@revolut.com>
2019-09-07 14:14:36 +0200

    Use project name in container name to uniquely name it

--- src/main/kotlin/com/revolut/jooq/JooqDockerPlugin.kt
+++ src/main/kotlin/com/revolut/jooq/JooqDockerPlugin.kt
@@ -6,7 +6,7 @@ import org.gradle.api.Project
 open class JooqDockerPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
-        project.extensions.create("jooq", JooqExtension::class.java)
+        project.extensions.create("jooq", JooqExtension::class.java, project.name)
         project.configurations.create("jdbc")
         project.tasks.create("generateJooqClasses", GenerateJooqClassesTask::class.java) {
             group = "jooq"
