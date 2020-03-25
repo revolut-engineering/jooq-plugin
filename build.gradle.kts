6af44dc7988aaf13fa78998b853e01ca095bfe42
Adrian Skrobacz <adrian.skrobacz@revolut.com>
2020-03-25 23:27:52 +0100

    Upgrade Gradle to version 6.3 along with other dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     id("java-gradle-plugin")
     id("com.gradle.plugin-publish") version "0.10.1"
     id("pl.droidsonroids.jacoco.testkit") version "1.0.5"
-    id("com.github.ben-manes.versions").version("0.27.0")
+    id("com.github.ben-manes.versions").version("0.28.0")
 }
 
 repositories {
@@ -95,7 +95,7 @@ tasks {
     }
 
     wrapper {
-        gradleVersion = "6.0.1"
+        gradleVersion = "6.3"
         distributionType = ALL
     }
 }
@@ -104,9 +104,9 @@ tasks {
 dependencies {
     implementation("org.jooq:jooq-codegen:3.13.1")
     implementation("org.glassfish.jaxb:jaxb-runtime:2.3.2")
-    implementation("com.github.docker-java:docker-java:3.1.5")
-    implementation("org.flywaydb:flyway-core:6.1.1")
-    implementation("org.testcontainers:testcontainers:1.12.4")
+    implementation("com.github.docker-java:docker-java:3.2.1")
+    implementation("org.flywaydb:flyway-core:6.3.2")
+    implementation("org.testcontainers:testcontainers:1.13.0")
 
     testImplementation("org.spockframework:spock-core:1.3-groovy-2.5")
     testCompileOnly(gradleTestKit())
