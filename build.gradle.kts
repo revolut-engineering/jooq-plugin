4c8eb9b33d93a5346a56be73a097799010ae145f
Sebastián <sebas@zaffarano.com.ar>
2022-12-06 14:48:50 +0100

    FINS-462 chore: Bump docker-java-transport-okhttp dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -126,7 +126,7 @@ afterEvaluate {
 dependencies {
     implementation("org.jooq:jooq-codegen:3.14.15")
     implementation("org.glassfish.jaxb:jaxb-runtime:2.3.3")
-    implementation("com.github.docker-java:docker-java-transport-okhttp:3.2.8")
+    implementation("com.github.docker-java:docker-java-transport-okhttp:3.2.14")
     implementation("org.flywaydb:flyway-core:6.4.3")
     implementation("org.zeroturnaround:zt-exec:1.12")
     compileOnly("javax.annotation:javax.annotation-api:1.3.2")
